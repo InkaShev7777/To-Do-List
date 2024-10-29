7f60e04e1db2cd576c4e76463891de7b90f120a9
Ilya Shevchenko <95874401+InkaShev7777@users.noreply.github.com>
2024-10-29 10:55:34 +0200

    init commit

---
+++ To Do List/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  To Do List
+//
+//  Created by Ilya Schevchenko on 29.10.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by Ilya Schevchenko on 29.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
