122d5b4c115c5c7b522f2ed3e50d21c564e37170
Ilya Shevchenko <95874401+InkaShev7777@users.noreply.github.com>
2024-10-29 11:42:46 +0200

    update struct of project and setting project for work with firebase

---
+++ To Do List/Views/MainView.swift
@@ -0,0 +1,24 @@
+//
+//  MainView.swift
+//  To Do List
+//
+//  Created by Ilya Schevchenko on 29.10.2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
